858fb1bef63bc7124f0dd49c92c49523e37ac180
Amy Kang <selene6023@gmail.com>
2014-09-22 00:11:16 -0700

    add data validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,7 @@ class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
 	default_scope -> {order('created_at DESC')}
+	validates :rating, :comment, presence: true
+	validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5,
+		message: "can only be a whole number between 1 and 5."}
 end
