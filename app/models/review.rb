55f0fb5a515beaeaae928862f6c56a6add86785c
Amy Kang <selene6023@gmail.com>
2014-09-22 00:19:35 -0700

    Only one review per restaurant

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,5 @@ class Review < ActiveRecord::Base
 	validates :rating, :comment, presence: true
 	validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5,
 		message: "can only be a whole number between 1 and 5."}
+	validates :user_id, uniqueness: {:scope => :restaurant_id, message: "may only write one review per restaurant."}
 end
